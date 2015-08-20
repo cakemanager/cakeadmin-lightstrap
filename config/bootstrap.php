4fd22655e267c9b0693d53cf0896e6b049bc0a5c
BobMulder <bobmulder@outlook.com>
2015-08-20 22:34:48 +0200

    Bootstrap plugin fix. Refering to #1

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,7 +16,7 @@ use Cake\Core\Configure;
 use Cake\Core\Plugin;
 use Settings\Core\Setting;
 
-Plugin::load('Bootstrap3');
+Plugin::load('Bootstrap');
 
 Configure::write('Settings.Prefixes.LS', 'LightStrap');
 
